34d3028309f1444f396e010f3d3776ee93b5663a
MiThetThetKhaing <mithetthetkhaing@acedatasystems.com>
2026-01-09 11:36:48 +0630

    feat: add Individual Loan Account Interest Detail

---
+++ src/features/reports/individual-loan-account-interest-detail/individual-loan-account-interest-detail.component.ts
@@ -0,0 +1,112 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { response } from 'express';
+import { saveAs } from 'file-saver';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-individual-loan-account-interest-detail',
+  imports: [
+      NgxExtendedPdfViewerModule,
+      MatCardModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatButtonModule,
+      MatSelectModule
+  ],
+  providers: [ DatePipe ],
+  templateUrl: './individual-loan-account-interest-detail.component.html',
+  styleUrl: './individual-loan-account-interest-detail.component.scss',
+})
+export class IndividualLoanAccountInterestDetailComponent {
+    src!: Blob;
+    fileName: string;
+
+    private url = environment.apiUrl;
+    private http = inject(HttpClient);
+    private fb = inject(FormBuilder);
+
+    protected readonly form: FormGroup = new FormGroup({});
+
+    // select box
+    format = new FormControl('');
+    formatList: string[] = ['PDF', 'EXCEL'];
+
+    constructor(private datePipe: DatePipe) {
+        this.form = this.fb.group({
+            startDate: ['', [Validators.required]],
+            endDate: ['', [Validators.required]],
+            format: ['', Validators.required]
+        });
+    }
+
+    onSubmit():void {
+        this.http
+            .post(
+                this.url + 'Report/GetIndividualLoanAccountInterestDetails', 
+                {
+                    startDate: this.datePipe.transform(this.form.value.startDate as Date, 'yyyy-MM-dd'),
+                    endDate: this.datePipe.transform(this.form.value.endDate as Date, 'yyyy-MM-dd'),
+                    format: (this.form.value.format as string).toLowerCase(),
+                    reportFullName: 'Individual_Loan_Account_Interest_Details.jrxml'
+                },
+                { observe: 'response', responseType: 'blob'}
+            )
+            .subscribe({
+                next: (response) => {
+                    this.fileName = response.headers.get('content-disposition')?.split('filename=')[1]?.trim().replace(/"/g, '') || 'unknown';
+
+                    if (this.form.value.format.toLowerCase() === 'pdf') {
+                        const blob = new File([response.body!], this.fileName, {
+                            type: 'application/pdf',
+                        });
+                        this.src = blob;
+                    }
+                    if (this.form.value.format.toLowerCase() === 'excel') {
+                        const blob = new File([response.body!], this.fileName, {
+                            type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        });
+                        saveAs(blob, this.fileName);
+                    }
+                    this.form.reset(this.form.value);
+                    this.form.markAsPristine();
+                    this.form.markAsUntouched();
+                }
+            })
+    }
+
+    get startDateIsRequired() {
+        return (
+            this.form.get('startDate')!.hasError('required') &&
+            this.form.get('startDate')!.touched
+        );
+    }
+
+    get endDateIsRequired() {
+        return (
+            this.form.get('endDate')!.hasError('required') &&
+            this.form.get('endDate')!.touched
+        );
+    }
+
+    get formatIsRequired() {
+        return (
+            this.form.get('format')!.hasError('required') &&
+            this.form.get('format')!.touched
+        );
+    }
+}
